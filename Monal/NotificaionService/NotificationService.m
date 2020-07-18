b7107ad549d77cd61e5d70b1d8fc71c3f9ad63cf
Thilo Molitor <thilo@eightysoft.de>
2020-07-18 12:03:53 +0200

    Add debug messages to notification service extension

--- Monal/NotificaionService/NotificationService.m
+++ Monal/NotificaionService/NotificationService.m
@@ -18,6 +18,7 @@
 @implementation NotificationService
 
 - (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
+    DDLogInfo(@"notification handler called");
     self.contentHandler = contentHandler;
     self.bestAttemptContent = [request.content mutableCopy];
     
@@ -25,10 +26,11 @@
     self.bestAttemptContent.title = @"New Message"; //[NSString stringWithFormat:@"New Message %@", self.bestAttemptContent.title];
     self.bestAttemptContent.body = @"Open app to view";
     self.bestAttemptContent.badge = @1;
-    self.contentHandler(self.bestAttemptContent);
+    //self.contentHandler(self.bestAttemptContent);
 }
 
 - (void)serviceExtensionTimeWillExpire {
+    DDLogInfo(@"notification handler expired");
     // Called just before the extension will be terminated by the system.
     // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
     self.contentHandler(self.bestAttemptContent);
